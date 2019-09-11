a30c2360556a43306676452a9dc90b6b277d9bf0
Gustaoliveira <gushoyer@hotmail.com>
2019-09-11 10:25:24 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,29 @@
+// This is a JavaScript file
+var Resultado;
+$(document).on("click","#calcular",function () {
+
+  var sexo = $("#sexo").val();
+  var peso = $("#peso").val();
+  var altura = $("#altura").val();
+  var idade = $("#idade").val();
+  var nivelAtividade = $("#nivelAtividade").val();
+
+
+  if (sexo == 'masculino') {
+    Resultado = nivelAtividade * (66 + (13, 7 * peso + (5 * altura - (6, 8 * idade))));
+  }
+  else if (sexo == 'feminino') {
+    Resultado = nivelAtividade * (655 + (13, 7 * peso + (5 * altura - (6, 8 * idade))));
+  }
+ $ ("#Resultado").val(Resultado);
+
+});
+var resertar;
+$(document).on("click", "#resertar", function () {
+
+  var peso = $("#peso").val("");
+  var altura = $("#altura").val("");
+  var idade = $("#idade").val("");
+  var Resultado = $("#Resultado").val("");
+  var sexo = $("#sexo").val("");
+});  
